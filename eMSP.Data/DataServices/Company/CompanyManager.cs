8d36239a44401d7a723a73e0b2449a2dfcbc6763
alexlaw <alexlaw6988@gmail.com>
2017-08-17 12:12:53 +0530

    Company Api, Data service changes, Raja

---
+++ eMSP.Data/DataServices/Company/CompanyManager.cs
@@ -0,0 +1,220 @@
+﻿using eMSP.ViewModel.MSP;
+using eMSP.Data.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eMSP.DataModel;
+
+namespace eMSP.Data.DataServices.Company
+{
+    public class CompanyManager : IDisposable
+    {
+
+        #region Initialization
+
+        private bool IsDisposed = false;
+
+        public CompanyManager()
+        {
+
+        }
+
+        #endregion
+
+        #region Get
+
+        public async Task<CompanyCreateModel> GetCompany(CompanyModel data)
+        {
+            try
+            {
+                CompanyCreateModel model = null;
+                long Id = Convert.ToInt64(data.id);
+                switch (data.companyType)
+                {
+                    case "MSP":
+                        tblMSPDetail dataMSP = await Task.Run(() => ManageMSP.GetMSPDetails(Id));
+                        model = dataMSP.ConvertTocompany();
+                        break;
+                    case "Customer":
+                        tblCustomer dataCustomer = await Task.Run(() => ManageCustomer.GetCustomerDetails(Id));
+                        model = dataCustomer.ConvertTocompany();
+                        break;
+                    case "Supplier":
+                        tblSupplier dataSupplier = await Task.Run(() => ManageSupplier.GetSupplierDetails(Id));
+                        model = dataSupplier.ConvertTocompany();
+                        break;
+                }
+
+                return model;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<CompanyCreateModel>> GetAllCompanies(CompanySearchModel Model)
+        {
+            try
+            {
+                List<CompanyCreateModel> res = null;
+
+                switch (Model.companyType)
+                {
+                    case "MSP":
+                        List<tblMSPDetail> dataMSP = await Task.Run(() => ManageMSP.GetAllMSPDetails(Model));
+                        res = dataMSP.Select(a => a.ConvertTocompany()).ToList();
+                        break;
+                    case "Customer":
+                        List<tblCustomer> dataCustomer = await Task.Run(() => ManageCustomer.GetAllCustomerDetails(Model));
+                        res = dataCustomer.Select(a => a.ConvertTocompany()).ToList();
+                        break;
+                    case "Supplier":
+                        List<tblSupplier> dataSupplier = await Task.Run(() => ManageSupplier.GetAllSupplierDetails(Model));
+                        res = dataSupplier.Select(a => a.ConvertTocompany()).ToList();
+                        break;
+                }
+
+                return res;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        #endregion
+
+        #region Insert
+
+        public async Task<CompanyCreateModel> CreateCompany(CompanyCreateModel data)
+        {
+            try
+            {
+                CompanyCreateModel model = null;
+
+                switch (data.companyType)
+                {
+                    case "MSP":
+                        tblMSPDetail dataMSP = await Task.Run(() => ManageMSP.InsertMSP(data.ConvertTotblMSPDetail()));
+                        model = dataMSP.ConvertTocompany();
+                        break;
+                    case "Customer":
+                        tblCustomer dataCustomer = await Task.Run(() => ManageCustomer.InsertCustomer(data.ConvertTotblCustomer()));
+                        model = dataCustomer.ConvertTocompany();
+                        break;
+                    case "Supplier":
+                        tblSupplier dataSupplier = await Task.Run(() => ManageSupplier.InsertSupplier(data.ConvertTotblSupplier()));
+                        model = dataSupplier.ConvertTocompany();
+                        break;
+                }
+
+                return model;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        #endregion
+
+        #region Update
+
+        public async Task<CompanyCreateModel> UpdateCompany(CompanyCreateModel data)
+        {
+            try
+            {
+                CompanyCreateModel model = null;
+
+                switch (data.companyType)
+                {
+                    case "MSP":
+                        tblMSPDetail dataMSP = await Task.Run(() => ManageMSP.UpdateMSP(data.ConvertTotblMSPDetail()));
+                        model = dataMSP.ConvertTocompany();
+                        break;
+                    case "Customer":
+                        tblCustomer dataCustomer = await Task.Run(() => ManageCustomer.UpdateCustomer(data.ConvertTotblCustomer()));
+                        model = dataCustomer.ConvertTocompany();
+                        break;
+                    case "Supplier":
+                        tblSupplier dataSupplier = await Task.Run(() => ManageSupplier.UpdateSupplier(data.ConvertTotblSupplier()));
+                        model = dataSupplier.ConvertTocompany();
+                        break;
+                }
+
+                return model;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        public async Task DeleteCompany(CompanyModel data)
+        {
+            try
+            {
+                long Id = Convert.ToInt64(data.id);
+                switch (data.companyType)
+                {
+                    case "MSP":
+                        await Task.Run(() => ManageMSP.DeleteMSP(Id));
+                        break;
+                    case "Customer":
+                        await Task.Run(() => ManageCustomer.DeleteCustomer(Id));
+                        break;
+                    case "Supplier":
+                        await Task.Run(() => ManageSupplier.DeleteSupplier(Id));
+                        break;
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region Dispose
+
+        protected virtual void Dispose(bool dispose)
+        {
+            if (!IsDisposed)
+            {
+                this.Dispose();
+            }
+
+            IsDisposed = true;
+        }
+
+        ~CompanyManager()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+
+
+
+}
+
+
+
---
+++ eMSP.Data/DataServices/Company/MSP/ManageMSP.cs
@@ -0,0 +1,140 @@
+﻿using eMSP.DataModel;
+using eMSP.ViewModel.MSP;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eMSP.Data.DataServices.Company
+{
+    internal static class ManageMSP
+    {
+        #region Initialization
+
+        internal static eMSPEntities mContext;
+
+        static ManageMSP()
+        {
+            mContext = new eMSPEntities();
+        }
+
+        #endregion
+
+        #region Get
+        internal static async Task<tblMSPDetail> GetMSPDetails(long Id)
+        {
+            try
+            {
+                using (var db = mContext)
+                {
+                    return await Task.Run(() => db.tblMSPDetails.Where(x => x.ID == Id).SingleOrDefault());
+
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+        }
+
+        internal static async Task<List<tblMSPDetail>> GetAllMSPDetails(CompanySearchModel model)
+        {
+            try
+            {
+                using (var db = mContext)
+                {
+                    return await Task.Run(() => db.tblMSPDetails.Where(x => x.CompanyName == model.companyName).ToList());
+
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+        }
+
+        #endregion
+
+        #region Insert
+
+        internal static async Task<tblMSPDetail> InsertMSP(tblMSPDetail model)
+        {
+            try
+            {
+                using (var db = mContext)
+                {
+                    model = db.tblMSPDetails.Add(model);
+
+                    int x = await Task.Run(() => db.SaveChangesAsync());
+
+                    return model;
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+        }
+
+        #endregion
+
+        #region Update
+
+        internal static async Task<tblMSPDetail> UpdateMSP(tblMSPDetail model)
+        {
+            try
+            {
+                using (var db = mContext)
+                {
+                    db.Entry(model).State = EntityState.Modified;
+
+                    int x = await Task.Run(() => db.SaveChangesAsync());
+
+                    return model;
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        internal static async Task DeleteMSP(long Id)
+        {
+            try
+            {
+                using (var db = mContext)
+                {
+                    tblMSPDetail obj = await db.tblMSPDetails.FindAsync(Id);
+                    db.tblMSPDetails.Remove(obj);
+                    int x = await Task.Run(() => db.SaveChangesAsync());
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+        }
+
+
+        #endregion
+
+
+    }
+}

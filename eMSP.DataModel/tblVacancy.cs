661d968c76c3088a0743da7468e4f38bcd8d0cb4
Alex Law <alexlaw6988@gmail.com>
2018-03-25 17:47:43 +0530

    Revert "file changes"
    This reverts commit da7d71025296cd2cb98ad2b5f13fe18af2283cc1.

--- eMSP.DataModel/tblVacancy.cs
+++ eMSP.DataModel/tblVacancy.cs
@@ -29,6 +29,7 @@ namespace eMSP.DataModel
     
         public long ID { get; set; }
         public short VacancyType { get; set; }
+        public Nullable<long> JobStatusID { get; set; }
         public long CustomerID { get; set; }
         public System.DateTime StartDate { get; set; }
         public Nullable<System.DateTime> EndDate { get; set; }
@@ -49,7 +50,6 @@ namespace eMSP.DataModel
         public string CreatedUserID { get; set; }
         public Nullable<System.DateTime> UpdatedTimestamp { get; set; }
         public string UpdatedUserID { get; set; }
-        public Nullable<long> JobStatusID { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<tblCandidateSubmission> tblCandidateSubmissions { get; set; }
